1c44d99e0ceb7c5431acb97ee2398e72938370b1
AmanuelD02 <amanueldebebe1000@gmail.com>
2021-12-15 21:43:15 +0300

    week 2 day 2

---
+++ week-2/find-original-array-from-doubled-array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        if len(changed) %2 ==1:
+            return []
+        changed.sort()
+        visited = {}
+        lst = []
+        for i in range(len(changed)):
+            if changed[i]/2 in visited:
+
+                lst.append(int(changed[i]/2))
+                visited[int(changed[i]/2)] -=1
+                if visited[int(changed[i]/2)] ==0:
+                    visited.pop(int(changed[i]/2))
+                
+                    
+                    
+            else:
+                if visited.get(changed[i]):
+                    visited[changed[i]] +=1
+                else:
+                    visited[changed[i]] = 1
+                            
+        if len(changed)/2 == len(lst):
+            
+            return lst
+        return []
