fe50f15fac86d067a16eb672ed5cf6e9193195a3
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2022-03-22 10:55:18 +0100

    Time: 45 ms (76.90%), Space: 13.9 MB (78.52%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -1,23 +1,17 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        occur = defaultdict(list)
-        start = i = 0
-        ans = []
-        for a,j in enumerate(s) :
-            occur[j].append(a)
+        ans, occur = [], {}
+        last = start = i = 0
         
-        last = occur[s[0]][-1]
-        while i < len(s):
-            # print(i, start, last)
+        for a,j in enumerate(s) :
+            occur[j] = a
+        
+        for i in range(len(s)):
+            last = max(last, occur[s[i]])
             if i == last:
                 ans.append(i - start +1)
                 start = last +1
-                if start <len(s):
-                    last = occur[s[start]][-1]
-            else:
-                last = max(last, occur[s[i]][-1])
-            
-            i+=1
+        
         return ans
         
         
