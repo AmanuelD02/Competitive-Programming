8cd4124f918b831476d3a6e1ada3357571273e83
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2022-04-08 10:28:45 +0200

    Time: 1395 ms (38.29%), Space: 20.9 MB (16.17%) - LeetHub

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        supplies = set(supplies)
+        output = []
+        visited = set()
+        graph = defaultdict(list)
+        
+        for i, food in enumerate(recipes):
+            graph[food] = ingredients[i]
+        
+        def dfs(food):
+            count = 0
+            visited.add(food)
+            for ingred in graph[food]:
+                if ingred in supplies:
+                    count += 1
+                else:
+                    if ingred not in visited:
+                        dfs(ingred)
+                        if ingred in supplies:
+                            count += 1
+            if count and count == len(graph[food]):
+                output.append(food)
+                supplies.add(food)
+        
+        
+        for f in recipes:
+            if f not in visited:
+                dfs(f)
+        
+        
+        return output
