6ec7009cc38396335aa78b78b2c2ba37da653489
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2022-04-08 10:29:49 +0200

    Time: 1110 ms (67.39%), Space: 21.6 MB (14.85%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -4,13 +4,13 @@ class Solution:
         output = []
         visited = set()
         graph = defaultdict(list)
-        
         for i, food in enumerate(recipes):
             graph[food] = ingredients[i]
         
         def dfs(food):
             count = 0
             visited.add(food)
+            
             for ingred in graph[food]:
                 if ingred in supplies:
                     count += 1
@@ -19,14 +19,13 @@ class Solution:
                         dfs(ingred)
                         if ingred in supplies:
                             count += 1
+            
             if count and count == len(graph[food]):
                 output.append(food)
                 supplies.add(food)
         
-        
         for f in recipes:
             if f not in visited:
                 dfs(f)
-        
         
         return output
