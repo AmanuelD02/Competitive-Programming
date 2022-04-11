638014c3c0d53212314a0940f93bc977cf0bb32d
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2022-04-11 17:15:56 +0200

    Time: 448 ms (22.28%), Space: 14.1 MB (89.14%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.py
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -2,14 +2,6 @@ class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
         M, N = len(grid), len(grid[0])
         
-        in_bound = lambda x, y: x < M and y< N
-        
-        def next_index(x,y):
-            if in_bound(x, y + 1): return (x, y + 1)
-            elif x + 1 < M:    
-                return (x+1, 1 - (N - y))
-            else:
-                return (0, 1 - (len(grid[0]) - y))
     
         temp = grid[0][0]        
         for _ in range(k):
