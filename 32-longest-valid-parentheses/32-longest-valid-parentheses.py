b0d1ac3b0cbaea03ad686e8e351a5b5b55a2714f
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2022-05-24 00:39:25 -0700

    Time: 74 ms (31.23%), Space: 15 MB (14.06%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.py
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.py
@@ -12,15 +12,12 @@ class Solution:
         if mergeInterval:
             longest = mergeInterval[0][1] - mergeInterval[0][0] + 1
         mergeInterval.sort()
-        # print(mergeInterval)
         l = 0
         r = 1
         while(r < len(mergeInterval)):
             if mergeInterval[l][1] + 1 >= mergeInterval[r][0]:
                 
                 mergeInterval[l] = (mergeInterval[l][0], max(mergeInterval[l][1], mergeInterval[r][1] ))
-                # print(max(mergeInterval[l][1], mergeInterval[r][1] ), l, mergeInterval[l])
-                # print(mergeInterval)
                 longest = max(longest, mergeInterval[l][1] - mergeInterval[l][0] + 1)
             else:
                 l = r    
