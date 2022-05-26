959c12ef22674f961a79c1c89de5fddbecbcfdbf
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2022-05-26 10:46:12 -0700

    Time: 40 ms (71.36%), Space: 14.2 MB (19.59%) - LeetHub

---
+++ 859-buddy-strings/859-buddy-strings.py
@@ -0,0 +1,21 @@
+class Solution:
+    def buddyStrings(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal) or len(s) == 1:
+            return False
+        
+        if s == goal:
+            visited = set()
+            for c in s:
+                if c in visited: return True
+                visited.add(c)
+            return False
+        
+        pairs = []
+        for a, b in zip(s, goal):
+            if a!=b:
+                pairs.append((a,b))
+            if len(pairs) > 2:
+                return False
+        if len(pairs) != 2: return False
+        
+        return pairs[0] == pairs[1][::-1]
