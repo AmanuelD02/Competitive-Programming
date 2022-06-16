752b5a43125c42c0806403fc0be0cd6e5f084336
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2022-06-16 02:44:22 -0700

    Time: 1454 ms (49.28%), Space: 14.1 MB (29.29%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,24 +1,27 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        longest = ""
-        length = 0
-        in_bound = lambda l, r: 0 <= l and  r < len(s)
-        #odd
+        N, longest = len(s), 0
+        word = ""
+        in_bound = lambda l, r: 0 <= l and r < N
+        
         for i in range(len(s)):
-            l = r = i
-            while in_bound(l,r) and s[l] == s[r]:
-                if length < (r - l + 1):
-                    longest = s[l:r + 1]
-                    length = r - l + 1
-                l -= 1 
-                r += 1
-        #even
-            l, r= i, i + 1
-            while in_bound(l,r) and s[l] ==s[r]:
-                if length < (r - l + 1):
-                    longest = s[l:r + 1]
-                    length = r - l + 1
-                l -= 1
-                r += 1
-        return longest
+            left = right = i
+            while in_bound(left, right) and s[left] == s[right]:
+                # print(left, right)
+                if (right - left + 1) > longest:
+                    word = s[left:right + 1]
+                    longest = right - left + 1
+                    # print("w",word)
+                left -= 1
+                right += 1
+            
+            left, right = i, i + 1
+            while in_bound(left, right) and s[left] == s[right]:
+                if (right - left + 1) > longest:
+                    word = s[left:right + 1]
+                    longest = right - left + 1
+                left -= 1
+                right += 1
+        # print(longest)
+        return word            
             
