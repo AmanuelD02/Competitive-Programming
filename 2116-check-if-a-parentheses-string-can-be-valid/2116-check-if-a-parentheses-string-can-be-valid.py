023dfe275f637a4deeb599d2cfef8ec40bb76527
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2022-09-14 16:57:26 +0300

    Time: 825 ms (5.22%), Space: 15.4 MB (46.65%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.py
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.py
@@ -1,29 +1,22 @@
 class Solution:
     def canBeValid(self, s: str, locked: str) -> bool:
         if len(s)%2 == 1: return False
-        flip= opn = close  = 0
-        for i, j in enumerate(s):
-            if locked[i] == '0':
-                flip += 1
-            elif j =='(':
-                opn += 1
-            else:
-                close += 1
+        firstRound = lambda opn, close, flip :  flip + opn < close
+        secondRound = lambda opn, close, flip:  flip + close < opn
+        def helper(s, locked, validator):
+            
+            flip= opn = close  = 0
+            for i, j in enumerate(s):
+                if locked[i] == '0':
+                    flip += 1
+                elif j =='(':
+                    opn += 1
+                else:
+                    close += 1
             
             # print("f round", opn, close, flip,i)
-            if flip + opn < close:
-                return False
-        flip= opn = close  = 0
-        locked = locked[::-1]
-        for i, j in enumerate(reversed(s)):
-            if locked[i] == '0':
-                flip += 1
-            elif j =='(':
-                opn += 1
-            else:
-                close += 1
-            
-            # print("s round", opn, close, flip,i)
-            if flip + close < opn: 
-                return False
-        return True
+                if validator(opn,close,flip):
+                    return False
+            return True
+        
+        return helper(s,locked,firstRound) and helper(s[::-1], locked[::-1],secondRound)
