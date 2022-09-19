6a1adb99847b847c8dee46906c9409bd0d61f19e
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2022-09-20 00:43:05 +0300

    Time: 532 ms (19.09%), Space: 15.7 MB (65.85%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.py
+++ 503-next-greater-element-ii/503-next-greater-element-ii.py
@@ -1,41 +1,16 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        Max = nums[0]
-        index = 0
-        for i, num in enumerate(nums):
-            if num >= Max:
-                Max =num
-                index = i
-
-        nextGreaterElement= [float("inf")] * n
-        nextGreaterIndex  = [-1] * n
-        for i in range(index-1, -1 ,-1):
-            cur= (i+1) % n
-            while nums[i] >=  nums[cur]:
-                if nextGreaterElement[cur]== float("inf"): break
-                cur = nextGreaterIndex[cur]
-            if nums[i] != nums[cur]:
-                nextGreaterElement[i] = nums[cur]
-            nextGreaterIndex[i] = cur
-    
-
-    
-        for i in range(n-1, index,-1):
-            cur= (i+1) % n 
-            while nums[i] >= nums[cur]:
-                if nextGreaterElement[cur]== float("inf"):
-                    
-                    break
-                cur = nextGreaterIndex[cur]
-            
-            if nums[i] != nums[cur]:
-                nextGreaterElement[i] = nums[cur]
-            nextGreaterIndex[i] = cur
+        stack = []
+        N = len(nums)
+        ans = [-1] * N
+        for n in range(2*N-1,-1,-1):
+            n = n % N
+            while stack and nums[stack[-1]] <= nums[n]:
+                stack.pop()
+            if stack: ans[n] = nums[stack[-1]]
+            stack.append(n)
         
-        for i, num in enumerate(nextGreaterElement):
-            if num == float("inf"):
-                nextGreaterElement[i] = -1
+
         
-        return nextGreaterElement
-            
+        return ans
+        
