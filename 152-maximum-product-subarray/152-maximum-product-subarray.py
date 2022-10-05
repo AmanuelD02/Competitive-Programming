ead62f73013e98c6e86340e1e1ca6ca6877552c8
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2022-10-05 21:34:46 +0300

    Time: 191 ms (24.74%), Space: 14.4 MB (38.01%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -1,22 +1,20 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        prefix, suffix = [1], [1]
+        prefix, suffix = 1, 1
         N = len(nums)
-        countL = countR = maxNum = maxLen =  0
-        
+        ans = -float("inf")
         for i in range(len(nums)):
             j = N - 1 - i
-            prevSum = prefix[-1] if prefix[-1] != 0 else 1
-            prefix.append(nums[i] * prevSum)
+            prevSum = prefix if prefix != 0 else 1
+            prefix = nums[i] * prevSum
+            
 
-            sufSum = suffix[-1] if suffix[-1] != 0 else 1
-            suffix.append(nums[j]* sufSum)
-          
+            sufSum = suffix if suffix != 0 else 1
+            suffix = nums[j]* sufSum
+            ans = max(ans,suffix, prefix)
         
-        # print(prefix[1:])
-        # print(suffix[1:])
-        return max(*suffix[1:],*prefix[1:])
-        # print(max(suffix))
-                           
+        
+        
+        return ans
                            
             
