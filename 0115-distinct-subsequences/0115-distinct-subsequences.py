47f58e0181796960ed61503b7c6cddf7d3bc2959
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2022-10-23 00:42:28 +0300

    Time: 1633 ms (35.88%), Space: 156.8 MB (14.62%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        
+        @lru_cache(None)
+        def dp(i, j):
+            if j >= len(t):
+                return 1
+            elif i >= len(s):
+                return 0
+            
+            
+            if s[i] == t[j]:
+                return dp(i + 1, j + 1) +  dp(i + 1, j)
+            
+            return dp(i + 1, j)
+        
+        return dp(0,0)
