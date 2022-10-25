78606b7485683f351f0b42bfa7e23282df788369
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2022-10-25 22:21:19 +0300

    Time: 1074 ms (97.68%), Space: 35.8 MB (54.04%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.py
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.py
@@ -1,30 +1,12 @@
 class Solution:
     def minCost(self, nums: List[int], cost: List[int]) -> int:
-        num_size = len(nums)
-        
         nums_cost = list(zip(nums, cost))
         nums_cost.sort()
         
-        prefix = [0] * num_size
-        suffix = [0] * num_size
-        totalCoin = 0
-        totalCoin2 = 0
-
-        for i in range(num_size -1, 0, -1):
-            j= num_size - i - 1
-            prefix[0] += (nums_cost[i][0] - nums_cost[0][0]) * nums_cost[i][1]
-            totalCoin += nums_cost[i][1]
-            
-            suffix[-1] += (nums_cost[-1][0] - nums_cost[j][0]) * nums_cost[j][1]
-            totalCoin2 += nums_cost[j][1]
-        
-        
-        for i in range(1, num_size -1):
-            j = num_size - i - 1
-            prefix[i] = prefix[i-1] - (nums_cost[i][0] - nums_cost[i-1][0])* totalCoin
-            totalCoin -= nums_cost[i][1]
-            
-            suffix[j] = suffix[j + 1] - (nums_cost[j + 1][0] - nums_cost[j][0]) * totalCoin2
-            totalCoin2 -= nums_cost[j][1]
-        
-        return min([ suffix[i] + prefix[i] for i in range(num_size)])
+        median = sum(cost) // 2
+        prefix = 0
+         
+        for num, cst in nums_cost:
+            prefix += cst
+            if prefix >= median:
+                return sum([abs(num - nums_cost[i][0]) * nums_cost[i][1] for i in range(len(nums))])
