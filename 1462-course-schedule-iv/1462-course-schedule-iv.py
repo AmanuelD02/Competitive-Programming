029eb089f69de95e3c72ec89cab1e04cf5ed49a0
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2022-10-27 11:29:11 +0300

    Time: 5875 ms (8.80%), Space: 17.7 MB (42.79%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,29 +1,15 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        graph, visited =  defaultdict(set), set()
-        lej = defaultdict(set)
+        graph = [[False]*(numCourses) for _ in range(numCourses)]
+        for x, y in prerequisites:
+            graph[x][y] = True
         
-        for pre, course  in prerequisites:
-            graph[pre].add(course)
-           
-        def dfs(node):
-            visited.add(node)
-            
-            for neigh in graph[node]:
-                if neigh not in visited:
-                    dfs(neigh)
-                lej[node].add(neigh)
-                lej[node].update( lej[neigh])
+        for k in range(numCourses):
+            for i in range(numCourses):
+                for j in range(numCourses):
+                    graph[i][j] = graph[i][j] or (graph[i][k] and graph[k][j])
         
-        for i in range(numCourses):
-            if i not in visited:
-                dfs(i)
+    
+        return [ graph[x][y] for x,y in queries]
+                               
         
-        ans = []
-        for pre, course in queries:
-            if course in lej[pre]:
-                ans.append(True)
-            else:
-                ans.append(False)
-        
-        return ans
