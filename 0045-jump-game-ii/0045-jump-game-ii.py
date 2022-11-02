08b4543af0b0367fe43f34b786edbfe0ff0a4633
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2022-11-02 22:19:34 +0300

    Time: 140 ms (90.69%), Space: 15.1 MB (56.20%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,15 +1,10 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        jumps = furthest = curEnd = 0
         
-        @lru_cache(None)
-        def dp(i):
-            if i >= len(nums)-1:
-                return 0
-            minMove = float("inf")
-            for j in range(nums[i],0,-1):
-                minMove = min(minMove, 1 + dp(i + j))
-            
-            
-            return minMove
-        
-        return dp(0)
+        for i in range(len(nums)-1):
+            furthest = max(furthest, i + nums[i])
+            if i == curEnd:
+                curEnd = furthest
+                jumps += 1
+        return jumps
