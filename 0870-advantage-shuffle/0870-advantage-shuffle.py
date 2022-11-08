5de7cee92e89b6f5d1bbcf030dca6ded890a9db6
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2022-11-08 12:04:56 +0100

    Time: 1573 ms (61.93%), Space: 34.6 MB (87.13%) - LeetHub

---
+++ 0870-advantage-shuffle/0870-advantage-shuffle.py
@@ -0,0 +1,28 @@
+class Solution:
+    def advantageCount(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        n = len(nums1)
+        
+        nums1.sort()
+        queue = deque(nums1)
+        Found = False
+        # print(queue)
+        # print(sorted(zip(nums2, range(n))))
+        for num, i in sorted(zip(nums2, range(n))):
+            if num < queue[0]:
+                nums2[i] = queue.popleft()
+            else:
+                # print("else")
+                if not Found:
+                    for j in range(len(queue)):
+                        if num >= queue[0]:
+                            queue.append(queue.popleft())
+                        else:
+                            break
+                    if j == len(queue):
+                        Found = True
+                nums2[i] = queue.popleft()
+            # print(num, i, queue)
+                
+        
+        return nums2
+        
