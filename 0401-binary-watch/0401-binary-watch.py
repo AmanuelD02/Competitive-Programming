2e229feccdd86cb1535f4a6f3c7c1a8127d508b5
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2022-11-08 15:52:12 +0100

    Time: 80 ms (9.28%), Space: 14 MB (28.65%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.py
+++ 0401-binary-watch/0401-binary-watch.py
@@ -1,53 +1,17 @@
 class Solution:
     def readBinaryWatch(self, turnedOn: int) -> List[str]:
-        lst = ['0'] * 10
-        self.validTimes = []
+        answer = []
         
-        self.backtrack(0, turnedOn, lst)
+        for h in range(12):
+            for m in range(60):
+                if self.countBits(h) + self.countBits(m) == turnedOn:
+                    answer.append("%d:%02d" % (h, m))
         
-        return self.validTimes
-        
-    def backtrack(self, i, led, lst):
-        if led == 0:
-            isValid = self.validate(lst)
-            
-            if isValid != None:
-                self.validTimes.append(isValid)
-            return
-        
-        if i == 10:
-            return
-        
-        
-        lst[i] = '1'
-        self.backtrack(i+1, led - 1, lst)
-        
-        lst[i] = '0'
-        self.backtrack(i+1, led, lst)
-        
-        
-            
+        return answer
     
-    def validate(self, lst):
-        hour = int(''.join(lst[:4]), 2)
-        minutes = int(''.join(lst[4:]), 2)
-        
-        if 0 <= hour <12 and 0 <= minutes <60:
-            ans = self.converter(hour, minutes)
-
-            return ans
-        
-        return None 
-            
-    
-    def converter(self, hour, minutes):        
-        ans = []
-            
-        ans.append(str(hour))
-        ans.append(':')
-        
-        if minutes < 10:
-            ans.append('0')
-        ans.append(str(minutes))
-        return "".join(ans)
-        
+    def countBits(self, num):
+        bits = 0
+        while num:
+            bits += (num & 1)
+            num >>= 1
+        return bits
