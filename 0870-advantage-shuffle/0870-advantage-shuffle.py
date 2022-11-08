4dcb494db72c956aa2024f640800b700972bd4d6
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2022-11-08 12:05:41 +0100

    Time: 1611 ms (60.05%), Space: 34.5 MB (87.13%) - LeetHub

--- 0870-advantage-shuffle/0870-advantage-shuffle.py
+++ 0870-advantage-shuffle/0870-advantage-shuffle.py
@@ -5,24 +5,22 @@ class Solution:
         nums1.sort()
         queue = deque(nums1)
         Found = False
-        # print(queue)
-        # print(sorted(zip(nums2, range(n))))
+        
         for num, i in sorted(zip(nums2, range(n))):
             if num < queue[0]:
                 nums2[i] = queue.popleft()
             else:
-                # print("else")
                 if not Found:
                     for j in range(len(queue)):
                         if num >= queue[0]:
                             queue.append(queue.popleft())
                         else:
                             break
+                            
                     if j == len(queue):
                         Found = True
-                nums2[i] = queue.popleft()
-            # print(num, i, queue)
                 
+                nums2[i] = queue.popleft()
         
         return nums2
         
