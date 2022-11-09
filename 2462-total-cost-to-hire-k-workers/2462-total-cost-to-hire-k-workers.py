b94e74af904e463333eba2498aa989764f44f080
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2022-11-09 21:47:34 +0100

    Time: 1546 ms (75.86%), Space: 29.8 MB (23.42%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.py
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.py
@@ -1,33 +1,32 @@
 class Solution:
     def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
-        # put (cost, index) as tuple in min heap
-        # initially heap will contain (candidates * 2) elements
-        # then the next k workers will be paid.
+        n = len(costs)
         
+        rightMost = n - 1 - candidates
+        leftMost =  candidates
         heap = []
-        L = len(costs)
-
-        for i, cost in enumerate(costs):
-            if i < candidates or L - 1 - i < candidates:
-                heappush(heap, [cost, i])
-
         
+        for i in range(n):
+            
+            if i < leftMost or i > rightMost:
+                heappush(heap, [costs[i], i])
+
         total_cost = 0
-        l = candidates
-        r = L - l - 1
-        while k:
+   
+        for _ in range(k):
             if heap:
                 curr = heappop(heap)
                 total_cost += curr[0]
-            
-            if l <= r < L:
-                if curr[1] <= l:
-                    heappush(heap, [costs[l], l])
 
-                    l += 1
-                elif curr[1] >= r:
-                    heappush(heap, [costs[r], r])
-                    r -= 1                
-            k -= 1
+                if leftMost <= rightMost:
+                    if leftMost >= curr[1]:
+                        heappush(heap, [costs[leftMost],leftMost])
+                        leftMost += 1
+
+                    elif rightMost <=  curr[1]:
+                        heappush(heap, [costs[rightMost], rightMost])
+                        rightMost -= 1
         
         return total_cost
+    
+            
