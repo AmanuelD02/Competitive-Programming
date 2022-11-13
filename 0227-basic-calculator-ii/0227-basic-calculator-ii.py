a1643171983276dada97779959d2529a07fc80cf
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2022-11-13 23:47:30 +0100

    Time: 851 ms (5.08%), Space: 20.3 MB (5.36%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,29 +1,16 @@
 class Solution:
-    
-    def evaluate(self, num1, num2, ops):
-        return eval(str(num1) + ops + str(num2))
-    
-    def convert(self, stack, ops):
-        newStack = []
-        i = 0
-        while i < len(stack):
-            if str(stack[i]) in ops:
-                num1 = newStack.pop()
-                num2 = stack[i + 1]
-                
-                res = self.evaluate(num1, num2, stack[i])
-                newStack.append(int(res))
-                i += 1
-            else:
-                newStack.append(stack[i])
-            
-            i += 1
-                
-        return newStack
-    
     def calculate(self, s: str) -> int:
-        stack = []
-        nums = []
+        stack  = self.stringToStack(s)
+        
+        
+        postfix = self.convertToPostFix(stack)
+        res = self.evaluatePostFix(postfix)
+        
+        return res
+
+        
+    def stringToStack(self, s: str) -> List:
+        stack, nums = [], []
         
         for i in s:
             if i.isdigit():
@@ -41,14 +28,50 @@ class Solution:
         if nums:
             stack.append(int("".join(nums)))
         
-        
-        newStack = self.convert(stack[:], "*/")
-        newStack2 = self.convert(newStack[:], "+-")
-        
-        return int(newStack2.pop())
-        
-
+        return stack
 
         
+    def convertToPostFix(self, stack):
+        ops = {"+", "-", "*", "/"}
+        resultStack, postFix = [], []
+        precedence = {}
+        
+        precedence['/'] =  precedence['*'] = 4
+        precedence['+'] =  precedence['-'] = 2
+        
+        for i, curr in enumerate(stack):
+            if str(curr) in ops:
+                while postFix and precedence[postFix[-1]] >= precedence[curr]:
+                    resultStack.append(postFix.pop())
+            
+                postFix.append(curr)
+            else:
+                resultStack.append(curr)
+                
+        postFix.reverse()
+        return resultStack + postFix
+    
+    
+    def evaluate(self, num1,num2, operator):
+        return int(eval(str(num1) + operator + str(num2)))
+                
+    
+    def evaluatePostFix(self, stack):
+        ops = {"+", "-", "*", "/"}
+        resultStack = []
+        
+        for curr in stack:
+            if curr in ops:
+                num1 = resultStack.pop()
+                num2 = resultStack.pop()
+                
+                res  = self.evaluate(num2, num1, curr)
+                
+                resultStack.append(res)
+                
+            else:
+                resultStack.append(curr)
+        
+        return resultStack.pop()
         
         
