a164b7e60fde0e1bfff2db1736f302fc045d16c9
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2023-02-02 23:28:50 +0300

    Time: 30 ms (91.74%), Space: 14.1 MB (32.94%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.py
+++ 0273-integer-to-english-words/0273-integer-to-english-words.py
@@ -1,30 +1,44 @@
 class Solution:
     def numberToWords(self, num: int) -> str:
-        toNineteen = {0:"", 1:"One", 2:"Two", 3:"Three", 4:"Four", 5:"Five", 6:"Six", 7:"Seven", 8:"Eight", 9:"Nine", 10:"Ten", 11:"Eleven", 12:"Twelve", 13:"Thirteen", 14:"Fourteen", 15:"Fifteen", 16:"Sixteen", 17:"Seventeen", 18:"Eighteen", 19:"Nineteen"}
+        toNineteen = {0:"", 1:"One", 
+                      2:"Two", 3:"Three",
+                      4:"Four", 5:"Five", 
+                      6:"Six", 7:"Seven",
+                      8:"Eight", 9:"Nine", 
+                      10:"Ten", 11:"Eleven", 
+                      12:"Twelve", 13:"Thirteen", 
+                      14:"Fourteen", 15:"Fifteen",
+                      16:"Sixteen", 17:"Seventeen", 
+                      18:"Eighteen", 19:"Nineteen"}
+        
         nums = [20, 100, 1000, 1000000, 1000000000]             
         
-        toNinety = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+        toNinety = ["", "", "Twenty",
+                    "Thirty", "Forty", "Fifty",
+                    "Sixty", "Seventy",
+                    "Eighty", "Ninety"]
    
         def helper(num):
             curr = ""
             if num < nums[0]:
                 curr = toNineteen[num]
+                
             elif num < nums[1]:
                 curr = toNinety[num // 10] + " " + toNineteen[num % 10]
+                
             elif num < nums[2]:
                 curr = helper(num // 100) + " Hundred " + helper(num % 100)
+                
             elif num < nums[3]:
                 curr = helper(num // 1000) + " Thousand " + helper(num % 1000)
+                
             elif num < nums[4]:
                 curr = helper(num // 1000000) + " Million " + helper(num % 1000000)
-            else:
                 
+            else:
                 curr = helper(num // 1000000000) + " Billion " + helper(num % 1000000000)
                 
             curr = curr.strip()
             return  curr
         
-        if num == 0:
-            return "Zero"
-    
-        return helper(num)
+        return helper(num) or "Zero"
