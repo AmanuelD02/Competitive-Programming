68da1cac428fab90f579f7f9d53b440277e88de3
AmanuelD02 <56916876+AmanuelD02@users.noreply.github.com>
2023-02-14 22:15:14 +0300

    Time: 2280 ms (5.30%), Space: 19.9 MB (10.26%) - LeetHub

---
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        N = len(s)
+        A = [0] * (N + 1)
+        B = [0] * (N  + 1)
+        
+        for i in range(N):
+            j = N - i - 1
+            B[i] = B[i - 1]
+            A[j] = A[min(j + 1, N - 1)]
+
+            if s[j] == 'a':
+                A[j] += 1
+            
+            if s[i] == 'b':
+                B[i] += 1
+        
+#         print(A)
+#         print(B)
+#         print("--------------")
+        # if A[0] == 0:
+        #     return 0
+        ans = float("inf")
+        for i in range(N + 1):
+            ans = min(ans, A[i] + B[i - 1])
+        
+        return ans
